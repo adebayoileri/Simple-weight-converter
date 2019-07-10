f128ebea84a6d3760a32f26d54c4cfbbca98948b
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2019-07-10 18:12:54 +0100

    changed file directory

--- Simple weight converter/app.js
+++
@@ -1,8 +0,0 @@
- document.getElementById("wrapper").style.visibility = "hidden";
- document.getElementById("value").addEventListener("input", function(e){
-    document.getElementById("wrapper").style.visibility = "";
-     let kgInput = e.target.value;
-     document.getElementById("gramResult").innerHTML = kgInput*1000;
-     document.getElementById("poundsResult").innerHTML = kgInput*2.205;
-     document.getElementById("Ounceresult").innerHTML = kgInput*35.2874;
- });
